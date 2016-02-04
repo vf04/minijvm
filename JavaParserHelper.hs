f8cb0dc847cfffdf3ebad606d8137b5fdb20518c
Jakob Herrmann <jakob.herrmann@infosprinter.com>
2016-02-04 02:40:40 +0100

    Minor change in JavaParserHelper

--- JavaParserHelper.hs
+++ JavaParserHelper.hs
@@ -1,7 +1,4 @@
 module JavaParserHelper where
 import AbsSyn
-fieldDeclToList :: FieldDecl -> [FieldDecl]
-fieldDeclToList(f) = [f]
-
-methodDeclToList :: MethodDecl -> [MethodDecl]
-methodDeclToList(m) = [m]
+fieldDeclToList(f) = ([f], [])
+methodDeclToList(m) = ([], [m])
